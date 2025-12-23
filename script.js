75620b81b96162ace3c36f1bfb38222e7de86794
Ayush Sahu <115640976+ayushS22@users.noreply.github.com>
2025-12-23 21:08:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
 function firstChar(text) {
-  // Loop through each character
   for (let i = 0; i < text.length; i++) {
-    // Check if character is NOT a space
     if (text[i] !== ' ') {
       return text[i];
     }
   }
-  // If no non-space character is found
   return '';
 }
-
-// Do not change the code below
-// const text = prompt("Enter text:");
-alert(firstChar(text));
